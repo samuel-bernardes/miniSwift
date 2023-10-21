7d16b33f351aed6b5bc26fab3fcf1b1479027e8c
samuel-bernardes <sbernardes467@gmail.com>
2023-10-21 12:26:59 -0300

    add new modes to interpreter

--- dist/lexical/LexicalAnalysis.js
+++ dist/lexical/LexicalAnalysis.js
@@ -1,30 +1,6 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LexicalAnalysis = void 0;
-const fs = __importStar(require("fs"));
 const Token_1 = require("./Token");
 const InternalException_1 = require("../error/InternalException");
 const Value_1 = require("../interpreter/value/Value");
@@ -89,8 +65,8 @@ const keywords = {
     'contains': Token_1.Token.TokenType.CONTAINS,
 };
 class LexicalAnalysis {
-    constructor(filename) {
-        this.input = fs.readFileSync(filename, 'utf8').split('');
+    constructor(input) {
+        this.input = input;
         this.line = 1;
         this.position = 0;
     }
--- dist/msi.js
+++ dist/msi.js
@@ -1,26 +1,97 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
+const fs = __importStar(require("fs"));
 const LexicalAnalysis_1 = require("./lexical/LexicalAnalysis");
 const SyntaticAnalysis_1 = require("./syntatic/SyntaticAnalysis");
 const Interpreter_1 = require("./interpreter/Interpreter");
+function streamToString(stream) {
+    var _a, stream_1, stream_1_1;
+    var _b, e_1, _c, _d;
+    return __awaiter(this, void 0, void 0, function* () {
+        // lets have a ReadableStream as a stream variable
+        const chunks = [];
+        try {
+            for (_a = true, stream_1 = __asyncValues(stream); stream_1_1 = yield stream_1.next(), _b = stream_1_1.done, !_b; _a = true) {
+                _d = stream_1_1.value;
+                _a = false;
+                const chunk = _d;
+                chunks.push(Buffer.from(chunk));
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (!_a && !_b && (_c = stream_1.return)) yield _c.call(stream_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return Buffer.concat(chunks).toString("utf-8");
+    });
+}
 function runPrompt() {
     const rl = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout,
     });
     rl.prompt();
-    rl.on('line', (line) => {
-        run(line);
+    let stringInput = "";
+    rl.on('line', (line) => __awaiter(this, void 0, void 0, function* () {
+        if (line === null) {
+            rl.close();
+            return;
+        }
+        stringInput = yield streamToString(line);
+        run(stringInput.split(''));
         rl.prompt();
+    }));
+    rl.on('close', () => {
+        process.exit(0);
     });
 }
 function runFile(filename) {
-    const fileStream = (0, fs_1.createReadStream)(filename);
-    run(fileStream);
+    run(fs.readFileSync(filename, 'utf8').split(''));
 }
-function run(inputStream) {
-    const lex = new LexicalAnalysis_1.LexicalAnalysis('teste.muel');
+function run(inputString) {
+    const lex = new LexicalAnalysis_1.LexicalAnalysis(inputString);
     const syntax = new SyntaticAnalysis_1.SyntaticAnalysis(lex);
     const cmd = syntax.process();
     Interpreter_1.Interpreter.interpret(cmd);
