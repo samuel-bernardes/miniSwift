a31c0baed7cf8bdecadc289edc42018aa0c2e20e
johmartinss <johndodo07@gmail.com>
2023-10-22 21:24:58 -0300

    for command working

--- src/interpreter/command/ForCommand.ts
+++ src/interpreter/command/ForCommand.ts
@@ -3,8 +3,10 @@ import { Expr } from "../expr/Expr";
 import { Variable } from "../expr/Variable";
 import { Category } from "../type/Type";
 import { BoolType } from "../type/primitive/types/BoolType";
+import { CharType } from "../type/primitive/types/CharType";
 import { Value } from "../value/Value";
 import { Command } from "./Command";
+import { InitializeCommand } from "./InitializeCommand";
 
 export class ForCommand extends Command {
 
@@ -22,27 +24,35 @@ export class ForCommand extends Command {
     //for let c : Char in word
 
     public execute(): void {
-
-        /* let value: Value = this.expr.expr();
-        let arrayType: ArrayType = ArrayType.instance(Category.Array, value.type);
-
-        if (arrayType.match(value.type)) {
-            valuiee
+        let value = this.expr.expr();
+        if(value.type.getCategory() == Category.Array){
+            let listItems: Value[] = value.data as Value[];
+            listItems.forEach(item => {
+                if(this.variable.getType() == item.type){
+                    this.variable.initialize(item);
+                    this.cmds.execute();
+                }
+                else{
+                    throw LanguageException.instance(super.getLine(), customErrors.InvalidType, this.variable.getType().toString()); 
+                }
+            });
         }
-
-        do {
-            let value: Value = this.expr.expr();
-            let boolType: BoolType = BoolType.instance();
-            if (boolType.match(value.type)) {
-                let b: boolean = Boolean(value.data);
-                if (!b)
-                    break;
-
-                this.cmds.execute();
-            } else {
-                throw LanguageException.instance(super.getLine(), customErrors["Tipo inválido"], value.type.toString());
+        else if (value.type.getCategory() == Category.String){
+            let str: String = value.data as String;
+            for (let i = 0; i < str.length; i++) {
+                let value: Value = new Value(CharType.instance(), str.charAt(i));
+                if(this.variable.getType() == CharType.instance()){
+                    this.variable.initialize(value);
+                    this.cmds.execute();
+                }
+                else{
+                    throw LanguageException.instance(super.getLine(), customErrors.InvalidType, this.variable.getType().toString()); 
+                } 
             }
-        } while (true); */
+        }
+        else{
+            throw LanguageException.instance(super.getLine(), customErrors.InvalidType, value.type.toString());
+        }
     }
 
 }
